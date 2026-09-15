fa2e52dfc3e5d5f53f9e3ce09e309a8191500e49
skyricksarkar <ricksarkar2005@gmail.com>
2026-09-15 16:31:59 +0530

    .

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express";
 
-const app = express;
+const app = express();
 
-app.use("*", (req, res) => {
-   return res.status(404);
+app.use((req, res) => {
+    return res.status(404).send("404 Not Found <br/> Coming soon");
 });
 
-app.listen(2000);
+app.listen(2001, () => {
+    console.log("Server running on http://localhost:2001");
+});
