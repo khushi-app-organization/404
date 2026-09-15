ca01d75617f5e2c13c6c387518a966a2a42ca681
skyricksarkar <ricksarkar2005@gmail.com>
2026-09-15 17:22:52 +0530

    .

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
+// Serve static assets (logo, etc.) from the public folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Send the "Coming Soon" page for any route
 app.use((req, res) => {
-    return res.status(404).send("404 Not Found <br/> Coming soon");
+    return res.status(200).sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(2001, () => {
